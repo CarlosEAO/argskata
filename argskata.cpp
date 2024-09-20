5e17482ca12e23ffc13feb8c0944119e39829bad
Carlos Aranda <carlos.eao15@gmail.com>
2024-09-19 23:42:44 -0600

    First OOP attempt

--- argskata.cpp
+++ argskata.cpp
@@ -2,7 +2,8 @@
 #include <iostream>
 #include <vector>
 #include <map>
-
+#include <unordered_map>
+#include <unordered_set>
 using namespace std;
 
 bool isnumeric(string s){
@@ -14,79 +15,172 @@ bool isnumeric(string s){
     
     return true; 
 }
-
-int main(){
-
-    map<char, string> schema;
-    
-    schema['b'] = "false";//bool
-    schema['i'] = "0";//int
-    schema['s'] = "";//string
-    schema['n'] = "0";//int
-    schema['f'] = "false";//bool
-
-    string args;
-    cout << "Introduce args string: "<<endl;
-    getline (cin, args);
-
+vector<string> splitSpace(string s){
     vector<string> tokens;
-
     int start, end = -1;
     do {
         start = end + 1;
-        end = args.find(" ", start);
-        tokens.push_back(args.substr(start, end - start));
+        end = s.find(" ", start);
+        tokens.push_back(s.substr(start, end - start));
     } while (end != -1);
+    return tokens;
+}
+class Parser{
+    public:
+        enum Type{INT, STRING, BOOL};
+    private:
+        string args;
+        unordered_map<char, Type> schema;
+        vector<string> tokens;
 
-    bool needFlag = true;
-    int type;//1 for number, 2 for string
-    char flag;
-    for (int i=0; i<tokens.size(); i++){
-        cout <<"token is "<< tokens[i] << endl;
-        //This indicates we have a flag (starts with -, not a number and size 1)
-        if(needFlag && tokens[i][0] == '-' && !isnumeric(tokens[i]) && tokens[i].size()==2){
-            flag = tokens[i][1];
-            switch (flag){
-                case 'b': case 'f':
-                    cout << "ITS A VALID FLAG"<<endl;
-                    schema[flag] = "true";
-                    break;
-                case 'i': case 'n':
-                    type = 1;
-                    needFlag = false;
-                    cout << "ITS A VALID FLAG"<<endl;
-                    break;
-                case 's':
-                    type = 2;
-                    needFlag = false;
-                    cout << "ITS A VALID FLAG"<<endl;
-                    break;
-                default: 
-                    cout << "UNKNOWN FLAG " << tokens[i][1]<<endl;
-            }
-        }
-        else if (!needFlag){
-            needFlag = true;
+        unordered_map<char, bool> booleanFlags;
+        unordered_map<char, int> intFlags;
+        unordered_map<char, string> stringFlags;
+
+        string typeAsString(Type type){
+            string typeString;
             switch (type){
-                case 1:
-                    if(isnumeric(tokens[i]))
-                        schema[flag] = tokens[i];
-                    else
-                        cout << "flag -" <<flag << " expects numeric value"<<endl; 
-                break;
-                case 2:
-                    schema[flag] = tokens[i];
-                break;
+                case BOOL:
+                    typeString = "boolean";
+                    break;
+                case INT:
+                    typeString = "int";
+                    break;
+                case STRING:
+                    typeString = "string";
+                    break;
+                default:
+                    typeString =  "UNKNOWN";
+                    break;
+            }
+            return typeString;
+        }
+
+        void addFlag(char identifier, Type type){
+            if(schema.find(identifier)!=schema.end()){
+                cout << "FLAG "<< identifier << " is already in the schema as a "<< typeAsString(schema[identifier]) <<" flag"<<endl;
+            }
+            else{
+                switch (type){
+                    case INT:
+                        schema[identifier] = INT;
+                        intFlags[identifier] = 0;
+                        break;
+                    case STRING:
+                        schema[identifier] = STRING;
+                        stringFlags[identifier] = "";
+                        break;
+                    case BOOL:
+                        schema[identifier] = BOOL;
+                        booleanFlags[identifier] = false;
+                        break;
+                }
             }
         }
-        else{
-            cout <<"FLAG MUST BEGIN WITH a - and be one char long"<<endl;
+    
+    public:
+        struct Flag {
+            char identifier; // The character identifier (e.g., 'i')
+            Type type;      // The type, but we will use it internally
+        };
+        void addBooleanFlag(char identifier){
+            addFlag(identifier, BOOL);
         }
-    }
+        void addIntFlag(char identifier){
+            addFlag(identifier, INT);
+        }
+        void addStringFlag(char identifier){
+            addFlag(identifier, STRING);
+        }
+        Parser(vector<pair<int, Type>> flags){
+            for( auto flag:flags){
+                addFlag(flag.first, flag.second);
+            }
+        }
+        void getArgs(){
+                cout << "Introduce args string: "<<endl;
+                getline (cin, this->args);
+        }
+        void parseArgs(string args){
+            vector<string> tokens = splitSpace(args);
+            bool expectingFlag = true;
+            Type type;
+            char flag;
+            for (int i=0; i<tokens.size(); i++){
+                cout <<"token is "<< tokens[i] << endl;
+                //This indicates we have a flag (starts with -, not a number and size 1)
+                if(expectingFlag && tokens[i][0] == '-' && !isnumeric(tokens[i]) && tokens[i].size()==2){
+                    flag = tokens[i][1];
+                    if(booleanFlags.count(flag)){
+                        cout << "ITS A VALID FLAG"<<endl;
+                        this->booleanFlags[flag] = true;
+                    }
+                    else if(intFlags.count(flag)){
+                        type = INT;
+                        expectingFlag = false;
+                        cout << "ITS A VALID FLAG"<<endl;
+                    }
+                    else if(stringFlags.count(flag)){
+                        type = STRING;
+                        expectingFlag = false;
+                        cout << "ITS A VALID FLAG"<<endl;
+                    }
+                    else{
+                        cout << "UNKNOWN FLAG " << tokens[i][1]<<endl;
+                    }
+                            
+                }
+                else if (!expectingFlag){
+                    expectingFlag = true;
+                    switch (type){
+                        case INT:
+                            if(isnumeric(tokens[i]))
+                                this->intFlags[flag] = stoi(tokens[i]);
+                            else
+                                cout << "flag -" <<flag << " expects numeric value"<<endl; 
+                            break;
+                        case STRING:
+                            stringFlags[flag] = tokens[i];
+                            break;
+                    }
+                }
+                else{
+                    cout <<"FLAG MUST BEGIN WITH a - and be one char long"<<endl;
+                }
+            }
+        }
 
-    map<char, string> :: iterator it;
-    for(it = schema.begin(); it != schema.end(); it++){
-        cout << "flag " << it->first << " value " << it->second << endl;
-    }
+        void parseArgs(){
+            this->parseArgs(args);
+        }
+
+        void printArgs(){
+            cout<<"Boolean Flags: "<<endl;
+            for(auto flag:booleanFlags){
+                cout << "flag " << flag.first << " value " << flag.second << endl;
+            }
+            cout<<"Integer Flags: "<<endl;
+            for(auto flag:intFlags){
+                cout << "flag " << flag.first << " value " << flag.second << endl;
+            }
+            cout<<"String Flags: "<<endl;
+            for(auto flag:stringFlags){
+                cout << "flag " << flag.first << " value " << flag.second << endl;
+            }
+        }
+};
+
+int main(){
+    Parser parser({{'i', Parser::INT}});
+    parser.addIntFlag('n');
+    parser.addBooleanFlag('b');
+    parser.addBooleanFlag('f');
+    parser.addStringFlag('s');
+    parser.addStringFlag('c');
+
+    parser.getArgs();
+    parser.parseArgs();
+    parser.printArgs();
+
     return 0;
 }
